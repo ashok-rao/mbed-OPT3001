b82cd9e28f23dc0f6cc4afe083cd0fcb288bd6f8
Ashok Rao <ashok.rao@arm.com>
2016-09-23 15:44:01 +0100

    Add files via upload

---
+++ OPT3001.cpp
@@ -0,0 +1,61 @@
+#include "OPT3001.h"
+
+uint16_t deviceID = 0, sensor_data;
+float sensor_data1 = 0.0;
+char temp_write[3] = {0,0,0};
+float lux_multiplier = 10.24; //Range set to RN[3:0] = 1010 in config register
+
+I2C i2c(PB_9, PB_8);
+
+void read_sensor()
+{
+    printf("Inside read sensor..");
+    i2c.frequency(400000);
+//Configure light sensor
+    temp_write[0]=0x01;
+    temp_write[1]=0xAE; //C810h is the default content for configuration register (address=01h) (AE0C)
+    temp_write[2]=0x0C;
+    
+    int a = i2c.write(address, temp_write, 3, 0);
+    //printf("a ====== %d",a);
+
+    //Read Device ID
+    temp_write[0] = 0x7F; //device ID register address
+    char data[2] = {0,0};
+    i2c.write(address, temp_write, 1, false);
+    int b = i2c.read(address, data, 2, 0);
+    //printf("b ======= =  == == %d", b);
+    deviceID = (((uint16_t)data[0]) << 8) | data[1];
+    //printf("Device ID= %d\n", deviceID);
+    if(deviceID == 0x3001) { //12289d (3001h) = device ID from datasheet
+        //led1 = 1;
+        //lcd.DisplayStringAt(2, LINE(3), (uint8_t *)"OK....talking to sensor", CENTER_MODE);
+        printf("Device ID OK.....");        
+    }
+    
+    //From datasheet: Read result register (mask bits 15:13 - AND with suitable mask) & Multiply contents by lux_multiply factor to get reading.
+    while(1) {
+        temp_write[0] = 0x00; //result register address
+        char opt_data[2] = {0,0};
+        i2c.write(address, temp_write, 1, false);
+        int c = i2c.read(address, opt_data, 2, 0);
+        //printf("opt_data[1] ==== %d\t", opt_data[1]);
+        //printf("opt_data[0] ==== %d\t", opt_data[0]);
+        //sensor_data = (((uint16_t)opt_data[0]) << 8) | opt_data[1];
+        sensor_data1 = ((((uint16_t)opt_data[0]) << 8) | opt_data[1]) & 0x1FFF;
+        //float sensor_data = (((((uint16_t)opt_data[1]) << 8)) & 0x1F00) | opt_data[0]; //check with AND operation...
+        //printf("Sensor data = %0.2f\n", sensor_data1);
+        sensor_data1 = sensor_data1 * lux_multiplier;
+        printf("Lux data = %f\n", sensor_data1);
+/*
+        if(sensor_data1 == 0) {
+            led1 = 1;
+            lcd.DisplayStringAt(3, LINE(4), (uint8_t *)"Too Dark..Turning ON LED", CENTER_MODE);
+        } else {
+            led1 = 0;
+            lcd.DisplayStringAt(3, LINE(4), (uint8_t *)"All OK. No lights needed", CENTER_MODE);
+        }
+        Thread::wait(1000);
+*/
+    }
+}
---
+++ OPT3001.h
@@ -0,0 +1,5 @@
+#include "mbed.h"
+
+const int address = 0x47 << 1;
+
+void read_sensor();
