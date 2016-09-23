ff0cf79792a00d1db8b75a3cf0c812177bfd9685
Ashok Rao <ashok.rao@arm.com>
2016-09-23 16:02:45 +0100

    Update OPT3001.h

--- OPT3001.h
+++ OPT3001.h
@@ -1,7 +1,7 @@
 #include "mbed.h"
 
-//I2C address of OPT3001 on the BOOSTXL-SENSORS breakout board from TI.
-//Please change this according to your board / I2C address allocated or required.
+//I2C address of OPT3001 on the BOOSTXL-SENSORS breakout board from TI is 0x47.
+//Please change this according to your board / I2C address allocated or required for your hardware.
 //mbed I2C API use 7 bit address, hence left shift the 8-bit address by 1 bit.
 const int address = 0x47 << 1;
 
